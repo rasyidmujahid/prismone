460e157df74ce5903b9cd007a4df470194efe599
rasyidmujahid <rasyidmujahid@github.com>
2014-03-23 16:06:50 +0700

    implement cc point

--- main.m
+++ main.m
@@ -1,11 +1,11 @@
-% Main
+%% Main
 
 [T, V] = stlreader('parts/0_001stl_ugpart2asc.stl');
 
-% find center of triangles
-% loop over triangles, foreach triangle T
-% foreach their vertices v
-% calculate their avarages point
+%% find center of triangles
+%% loop over triangles, foreach triangle T
+%% foreach their vertices v
+%% calculate their avarages point
 for i = 1:size(T,1)
     vid1 = T(i,1);  % first vertex ID
     vid2 = T(i,2);
@@ -34,10 +34,9 @@ zlabel ( '--Z axis--' )
 
 hold on;
 
-% surfnorm( T(:,4), T(:,5), T(:,6) );
+%% plot normal vector along with triangle surface
+%quiver3( tricenter(:,1), tricenter(:,2), tricenter(:,3), T(:,4), T(:,5), T(:,6) );
 
-% plot normal vector along with triangle surface
-quiver3( tricenter(:,1), tricenter(:,2), tricenter(:,3), T(:,4), T(:,5), T(:,6) );
+ccp = ccpoint(T(:,1:3), V, 2);
 
-% proceed to bucketing
-% then slicing
+plot3(ccp(:,1), ccp(:,2), ccp(:,3), 'o');
