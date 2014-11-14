d4597c03d7d8907440076f240baa60c13b73e3f8
rasyidmujahid <rasyidmujahid@github.com>
2014-11-14 07:38:19 +0700

    add orientation logic

--- cbv-projects/orientation.m
+++ cbv-projects/orientation.m
@@ -1,8 +1,30 @@
-%% orientation: set ccpoint orientation
-%% 
+%% orientation:		set ccpoint orientation
+%% arguments: 		@points roughing points that want to find their tool orientation
+%%					@boundary_points intersection points of vertical slicing lines and 
+%%					model that formed a boundar points 
+%% returns:			each point in @points + orientation vector
 function outputs = orientation(points, boundary_points)
 	outputs = [];
-	turning_point_before_cbv();
+
+
+	%% algorithm:
+	%% 1. loop over ccpoints under CBV
+	%% 2. foreach the ccpoint, find neighbour ccpoint that fall outside CBV
+	%%    How to find that neighbour ccp:
+	%%    a. loop over all ccp along x,y,z coordinate
+	%%    b.        .  .
+	%%              . .
+	%%    ..........o...........>
+	%%            . .
+	%%           .  .
+	%% 3. if ccp outside CBV found, take its max z-upper as end-point to where 
+	%%    ccp will skew its orientation
+	%% 4. check for gouging, if it is, take lower z-upper
+	%% 5. done
+
+
+
+	turning_points = [];
 	for i = 1:size(points,1)
 		point = points(i,:);
 		if is_under_cbv(point, get_boundary_points_at(point(1:2), boundary_points))
@@ -14,8 +36,11 @@ function outputs = orientation(points, boundary_points)
 	end
 end
 
-%% skewed_orientation: build a vector from this point and another point outside cbv closest to this point
-%% returns: orientation vector thru ccp outside cbv
+%% skewed_orientation: 	build a vector from this point towards another point outside cbv 
+%%						closest to this point
+%% arguments:			@point point that want to find its vector
+%%						@boundary_points intersection points
+%% returns: 			orientation vector thru ccp outside cbv
 function orientation_vector = skewed_orientation(point, boundary_points)
 	ccp_outside_cbv = find_closest_ccp_outside_cbv(point, boundary_points);
 	orientation_vector = [];
@@ -25,6 +50,12 @@ end
 function closest_ccp = find_closest_ccp_outside_cbv(point, boundary_points)
 
 	closest_ccp = [];
+end
+
+%% turning_point_before_cbv: find slicing line right before enter cbv
+function output = turning_points_before_cbv(boundary_points)
+
+	output = [];
 end
 
 %% get_boundary_points_at: get boundary points at specified slicing line
@@ -49,9 +80,3 @@ function inside = is_under_cbv(point, boundary_at_this_slicing_line)
         inside = false;
     end
 end
-
-%% turning_point_before_cbv: find slicing line right before enter cbv
-function output = turning_point_before_cbv(boundary_points)
-	output = [];
-end
-
