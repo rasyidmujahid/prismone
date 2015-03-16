a7f647031771ca64965218603cb3e1ae6507f5c8
rasyidmujahid <rasyidmujahid@github.com>
2015-03-17 05:21:30 +0700

    bucket

--- flank/+bucket/Buckets.m
+++ flank/+bucket/Buckets.m
@@ -1,16 +1,16 @@
 classdef Buckets < handle
-	properties
-		bucket
-	end
+    properties
+        bucket
+    end
 
-	methods
-		function buckets = Buckets(len, width)
-			buckets(len, width) = Buckets;
-			for l = 1:len
-				for w = 1:width
-					buckets(l,w).bucket = bucket.Bucket(l,w);
-				end
-			end
-		end
-	end
+    methods
+        function buckets = Buckets(len, width)
+            buckets(len, width) = Buckets;
+            for l = 1:len
+                for w = 1:width
+                    buckets(l,w).bucket = bucket.Bucket(l,w);
+                end
+            end
+        end
+    end
 end
--- flank/+bucket/Builder.m
+++ flank/+bucket/Builder.m
@@ -1,33 +1,50 @@
 classdef Builder < handle
-	properties
-		buckets
-		triangles
-		vertices
-		width
-	end
+    properties
+        buckets
+        triangles
+        vertices
+        width
+    end
 
-	methods
-		function builder = Builder(triangles, vertices, width)
-			builder.triangles = triangles;
-			builder.vertices = vertices;
-			builder.width = width
+    properties(SetAccess = private)
 
-			minmax = bucket.Util.minmax(vertices);
-			builder.create_buckets(minmax)
-			bucketize
+    end
 
-		end
+    methods
+        function builder = Builder(triangles, vertices, width)
+            builder.triangles = triangles;
+            builder.vertices = vertices;
+            builder.width = width
 
-		function create_buckets(builder, minmax)
-			len_x = ceil((minmax(2,1) - minmax(1,1)) / builder.width);
-			len_y = ceil((minmax(2,2) - minmax(1,2)) / builder.width);
+            minmax = bucket.Util.minmax(vertices);
+            builder.create_buckets(minmax)
+            bucketize
+        end
 
-			builder.buckets = bucket.Buckets(len_x, len_y);
-		end
+        function create_buckets(builder, minmax)
+            len_x = ceil((minmax(2,1) - minmax(1,1)) / builder.width);
+            len_y = ceil((minmax(2,2) - minmax(1,2)) / builder.width);
 
-		function bucketize
-			
-		end
+            builder.buckets = bucket.Buckets(len_x, len_y);
+        end
 
-	end
+        function bucketize(builder)
+            for i = 1:size(builder.triangles, 1)
+                vertex_indices      = builder.triangles(i,:);
+                triangle_vertices   = builder.vertices(vertex_indices,:);
+                identified_buckets  = find_bucket_for(triangle_vertices);
+
+            end
+        end
+
+        %% find bucket for given triangle provided its vertices.
+        %% A triangle can be under several bucket (at most 4).
+        %% 
+        %% Returns bucket object.
+        function find_bucket_for(builder, triangle_vertices)
+            in_buckets = [];
+            
+        end
+
+    end
 end
