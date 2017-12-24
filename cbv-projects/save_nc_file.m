a979a1b196729f6ff29def5aceeb8501a53af61f
jahid <rasyidmujahid@gmail.com>
2017-12-25 03:06:53 +0700

    remove sort on writing nc file

--- cbv-projects/save_nc_file.m
+++ cbv-projects/save_nc_file.m
@@ -12,8 +12,6 @@ function output = save_nc_file(X, Y, Z, i, j, k, lx, ly, lz, lt, tilting_type, f
         throw(ME); 
     end
 
-    % output = sortrows(output, [2 1]);
-
     %% write to file [x y z i j k]
     fileID = fopen([filename, '.nc'], 'w');
     for i = 1:size(output,1)
